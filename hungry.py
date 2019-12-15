cf422dd68fb162787294ed06769d3c0821813515
Itachi-Uchia <rahul18siyer@gmail.com>
2019-12-15 17:10:57 +0530

    prime addition

--- hungry.py
+++ hungry.py
@@ -4,3 +4,10 @@ if a==0:
     print('we are good')
 else:
     print('new commit')
+def prime():
+    n=3
+    if n%2==0:
+        print ('not a prime number')
+        
+    else:
+        print('prime number')
